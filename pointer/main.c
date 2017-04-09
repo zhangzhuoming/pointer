b47c977670c578aeffaef544405ee47fddf7e3d8
zhang <zhang@DESKTOP-8NUEC8D>
2017-04-10 00:09:12 +0800

    new

--- pointer/main.c
+++ pointer/main.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 void main()
 {
-	int i, a[10], *p;
+	extern exchange(int str[], int *p);
+	int i,a[10] = {0,1,2,3,4,5,6,7,8,9}, *p;
 	p = a;
+	exchange(a,p);
 	for (i = 0; i < 10; i++)
 	{
-		scanf("%d", p++);
-	}
-	printf("\n");
-	for (i = 0; i < 10; i++)
-	{
-		p--;
-		printf("%d", *p);
+		printf("%d", *(p++));
 	}
 	system("pause");
 }
